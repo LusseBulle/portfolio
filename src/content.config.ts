0bf7b51fd31dcd652ab41cba4692c534e7051737
Lucas Eriksson <lussebulleg@gmail.com>
2026-06-18 18:27:52 +0200

    inital work on project. dirty copy of blog entry

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,4 +17,17 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+const project = defineCollection({
+	loader: glob({ base: './src/content/project', pattern: '**/*.{md,mdx}' }),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: z.optional(image()),
+	}),
+});
+
+export const collections = { blog, project };
---
+++ src/content/project/semantic-algorithm.md
@@ -0,0 +1,7 @@
+---
+title: 'Semantic Sliding-Window Algorithm'
+description: "Bachelor's thesis work"
+pubDate: 'Jun 12 2026'
+---
+
+Testing
